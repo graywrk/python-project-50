8e1e4feefbe01bb3bb63aec0a2ba03d6ee33b204
Sergei Dmitriev <sergei.dmitriev@vk.team>
2023-09-30 12:17:28 +0300

    Step 5

--- gendiff/core.py
+++ gendiff/core.py
@@ -1,23 +1,51 @@
 import json
+import yaml
+
+
+def load_from_json(file_path):
+    with open(file_path) as file:
+        result_dict = json.load(file)
+    return result_dict
+
+
+def load_from_yaml(file_path):
+    with open(file_path) as file:
+        result_dict = yaml.safe_load(file)
+    return result_dict
+
+
+def load_from_file(file_path):
+    if file_path.endswith('.yaml') or file_path.endswith('.yml'):
+        return load_from_yaml(file_path)
+    elif file_path.endswith('.json'):
+        return load_from_json(file_path)
+    else:
+        raise Exception('Unknown file format')
+
+
+def make_diff_string(key, value, prefix="    "):
+    return prefix + str(key) + ": " + str(value) + "\n"
 
 
 def generate_diff(file_path1, file_path2):
-    with open(file_path1) as file1, open(file_path2) as file2:
-        dict1 = json.load(file1)
-        dict2 = json.load(file2)
+    dict1 = load_from_file(file_path1)
+    dict2 = load_from_file(file_path2)
+
     result = "{\n"
+
     allkeys = {key for dictionary in [dict1, dict2] for key in dictionary}
     allkeys = sorted(allkeys)
+
     for key in allkeys:
         if key in dict1 and key in dict2:
             if dict1[key] == dict2[key]:
-                result += "    " + str(key) + ": " + str(dict1[key]) + "\n"
+                result += make_diff_string(key, dict1[key])
             else:
-                result += "  - " + str(key) + ": " + str(dict1[key]) + "\n"
-                result += "  + " + str(key) + ": " + str(dict2[key]) + "\n"
+                result += make_diff_string(key, dict1[key], "  - ")
+                result += make_diff_string(key, dict2[key], "  + ")
         elif key in dict1:
-            result += "  - " + str(key) + ": " + str(dict1[key]) + "\n"
+            result += make_diff_string(key, dict1[key], "  - ")
         elif key in dict2:
-            result += "  + " + str(key) + ": " + str(dict2[key]) + "\n"
+            result += make_diff_string(key, dict2[key], "  + ")
     result += "}"
     return result
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,9 +6,18 @@ from gendiff import generate_diff
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_gendiff():
+def test_gendiff_json():
     file1_path = CURRENT_DIR + "/fixtures/file1.json"
     file2_path = CURRENT_DIR + "/fixtures/file2.json"
+    result_file_path = CURRENT_DIR + "/fixtures/result.txt"
+    with open(result_file_path) as result_file:
+        result = result_file.read()
+    assert generate_diff(file1_path, file2_path) == result
+
+
+def test_gendiff_yaml():
+    file1_path = CURRENT_DIR + "/fixtures/file1.yaml"
+    file2_path = CURRENT_DIR + "/fixtures/file2.yaml"
     result_file_path = CURRENT_DIR + "/fixtures/result.txt"
     with open(result_file_path) as result_file:
         result = result_file.read()
