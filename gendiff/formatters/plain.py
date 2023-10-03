592961047a374a79dfe039019198ad661544213f
Sergei Dmitriev <sergei.dmitriev@vk.team>
2023-10-03 17:49:55 +0300

    Fix for hexlet tests

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -14,20 +14,20 @@ def normalize_value(value):
 
 
 def format_plain(AST, path=[]): # noqa: max-complexity=10
-    result = ""
+    result = []
     for key in AST:
         if len(AST[key]['children']) > 0:
-            result += format_plain(AST[key]['children'], path + [key])
+            result.append(format_plain(AST[key]['children'], path + [key]))
             continue
         if AST[key]['type'] == 'UNCHANGED':
             pass
         elif AST[key]['type'] == 'CHANGED':
-            result += "Property '%s' was updated. From %s to %s\n" % ('.'.join(path + [key]), normalize_value(AST[key]['value']), normalize_value(AST[key]['new_value']))
+            result.append("Property '%s' was updated. From %s to %s" % ('.'.join(path + [key]), normalize_value(AST[key]['value']), normalize_value(AST[key]['new_value'])))
         elif AST[key]['type'] == 'DELETED':
-            result += "Property '%s' was removed\n" % ('.'.join(path + [key]),)
+            result.append("Property '%s' was removed" % ('.'.join(path + [key]),))
         elif AST[key]['type'] == 'ADDED':
-            result += "Property '%s' was added with value: %s\n" % ('.'.join(path + [key]), normalize_value(AST[key]['value']))
+            result.append("Property '%s' was added with value: %s" % ('.'.join(path + [key]), normalize_value(AST[key]['value'])))
         else:
             raise Exception("Not supported key type")
 
-    return result
+    return "\n".join(result)
